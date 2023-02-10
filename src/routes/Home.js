3acc2c87c938425347f28fdc562ccb2d8fd13668
Ankush <ankushsonkar987@gmail.com>
2023-02-11 01:25:12 +0530

    added image in hcontent

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Hcontent from "../components/Hcontent";
 import Navbar from "../components/Navbar";
-
+import img from "../assets/img.jpg"
 
 const Home = () => {
   return (
     <>
       <div className="home-section">
-        <Hcontent name="Ankush Sonkar" work="Web Developer & UX/UI Designer" image="#"/>
+        <Hcontent name="Ankush Sonkar" work="Web Developer & UX/UI Designer" image={img}/>
         <Navbar />
       </div>
     </>
