9188e40dfe89c3311b345be889f54bb0ba1cf70f
Ankush <ankushsonkar987@gmail.com>
2023-02-10 13:19:10 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
